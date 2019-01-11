392917e14a6bb5c8c894176950529075bba599dc
chrycko9491@gmail.com <chrycko9491@gmail.com>
2019-01-10 23:05:47 -0500

    Setting up to make an app to monitor my heating and allow for better optimization of resources.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace asp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
